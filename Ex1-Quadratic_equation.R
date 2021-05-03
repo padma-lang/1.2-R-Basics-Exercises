3d4ed953813ae456266fc80f51980bdecc9f7f8b
Pamidip <pamidip@hotmail.com>
2021-05-03 12:21:33 -0500

    first commit

---
+++ Ex1-Quadratic_equation.R
@@ -0,0 +1,16 @@
+a <- 2
+b <- 3
+c <- 1
+
+print(a)
+print(b)
+print(c)
+
+ls()
+
+x <- (-b + sqrt(b^2 - 4*a*c))/(2*a)
+
+y <- (-b - sqrt(b^2 - 4*a*c))/(2*a)
+
+print(x)
+print(y)
---
+++ RBasics-Ex1-assignment-Sum_of_firstn_numbers.R
@@ -0,0 +1,10 @@
+#compute the sum for the first 20 integers
+#formula n(n+1)/2
+
+n <- 20
+n*(n+1)/2
+
+# first 100 numbers
+
+n <-100
+n*(n+1)/2
